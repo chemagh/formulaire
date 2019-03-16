f873b9fa9db359a86443e52bbab6ea8bf21baa12
chemagh <44727828+chemagh@users.noreply.github.com>
2019-03-16 18:10:45 +0100

    Add files via upload

---
+++ formulaire/js/main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("myDate").value;
+    document.getElementById("demo").innerHTML = x;
+  }
+function myFunction() {
+    var x = document.getElementById("Fullname").name;
+    document.getElementById("demo").innerHTML = x;
+function myFunction() {
+        var x = document.getElementById("email").name;
+        document.getElementById("demo").innerHTML = x;
